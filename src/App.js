68254dd6617adc6ac40df9895655989da1efebd6
saidtribu <said@tribuemprendedores.com>
2021-02-12 21:22:09 -0800

    finish project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import {CardLists} from './components/card-list/card-list.component';
 import './App.css';
+import {SearchBox} from './components/search-box/search-box.component';
+class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      monsters : [],
+      searchField: ''
+    };
+
+    
+  }
+
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then (response => response.json())
+  .then(users => this.setState({ monsters: users}));
+}
+
+ handleChange = (e) => {
+   this.setState({searchField: e.target.value})
+ }
+
+  render() {
+    const {  monsters, searchField } =  this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      
+      );
+    return (
+          <div className="App">
+                    <h1>Monster Rolodex</h1>
+                      <SearchBox
+                        placeholder="search monsters"
+                        handleChange={this.handleChange}  
+                      />
+                     <CardLists monsters={filteredMonsters}  />  
+          </div>
+        );
+  }
+}
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +67,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
